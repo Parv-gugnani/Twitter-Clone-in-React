d2662fec014fb6a956b73fa4068116fea86d6d1d
Parv-gugnani <9765.parv@gmail.com>
2023-10-16 12:17:32 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Sidebar from "./Sidebar.js";
+import TweetBox from "./TweetBox";
 
 function App() {
   return (
     <div className="App">
       <Sidebar /> {/* Render the Sidebar component here */}
+      <TweetBox />
     </div>
   );
 }
